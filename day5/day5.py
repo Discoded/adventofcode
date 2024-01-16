cb4ae0845e63198775f21f2b62c7d50ea1dd3193
Discoded <33738298+Discoded@users.noreply.github.com>
2024-01-16 12:29:24 -0800

    day5

---
+++ day5/day5.py
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2023/day/5
+
+import re
+try: 
+    # Open file containing the words separated by a line
+    theFile = open("./day2.txt")
+    #theFile = open("./message.txt")
+    #theFile = open("./day2-practice.txt")
+    
+except:
+    print("NO FILE")
+
+def main():
+    load_data()
+
+main()
